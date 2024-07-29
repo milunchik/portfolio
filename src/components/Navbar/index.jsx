0c01415bc968401637c25040af3dea3c13b88c58
Emily <emili061116@gmail.com>
2024-07-29 22:34:01 +0300

    add navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,12 +1,54 @@
-import { React } from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
-// import { SiWebmoney } from "react-icons/si";
+import { SiWebmoney } from "react-icons/si";
+import { menu } from "../../data";
+import { Link } from "react-scroll";
+import { FaArrowUpRightFromSquare, FaBarsStaggered } from "react-icons/fa6";
+import { FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
+  const [showSidebar, setShowSideBar] = useState(false);
   return (
     <nav className="navbar__container">
-      <div className="log__container ">{/* <SiWebmoney /> */}</div>
-      <div className="tab__group"></div>
+      {showSidebar ? (
+        <div className="overlay" onClick={() => setShowSideBar(!showSidebar)}>
+          <FaTimes />
+        </div>
+      ) : (
+        ""
+      )}
+      <div className="logo__container">{<SiWebmoney />}</div>
+      <div className={`tab__group ${showSidebar ? "show" : ""}`}>
+        <div
+          className="span.icon__container close__btn"
+          onClick={() => setShowSideBar(!showSidebar)}
+        >
+          <FaTimes />
+        </div>
+        {menu.map((list, index) => (
+          <Link
+            activeClass="active"
+            className="tab__item name"
+            to={list.name.toLowerCase()}
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+            key={index}
+          >
+            {list.name}
+          </Link>
+        ))}
+      </div>
+      <div className="nav__button__group">
+        <button className="btn btn__primary">
+          Hire Me <FaArrowUpRightFromSquare />
+        </button>
+        <FaBarsStaggered
+          className="menu"
+          onClick={() => setShowSideBar(!showSidebar)}
+        />
+      </div>
     </nav>
   );
 };
