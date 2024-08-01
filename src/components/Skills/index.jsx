c1ddb69f8bb175764a3463378f5663a26e73de6d
Emily <emili061116@gmail.com>
2024-08-01 14:21:22 +0300

    add skills & skillCard

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,11 +1,22 @@
-import { React } from "react";
+import React from "react";
 import "./Skills.css";
+import { experience } from "../../data";
+import SkillCard from "./SkillCard";
 
 const Skills = () => {
   return (
-    <div>
-      <h1>This is Skills</h1>
-    </div>
+    <section id="skills">
+      <div className="section__wrapper">
+        <div className="section__header center">
+          <h2 className="primary__title">My Skills</h2>
+        </div>
+        <div className="skill__container">
+          {experience.map((list, index) => (
+            <SkillCard {...list} key={index} />
+          ))}
+        </div>
+      </div>
+    </section>
   );
 };
 
